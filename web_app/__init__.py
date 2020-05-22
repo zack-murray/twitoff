4b9462b087934c566a398ea1cc83252155ab0f6c
Zack Murray <zachery.murray@gmail.com>
2020-05-21 20:50:06 -0400

    final commit for heroku deployment

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,6 +1,8 @@
 # web_app/__init__.py
-
+import os
+from dotenv import load_dotenv
 from flask import Flask
+
 
 from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
@@ -9,12 +11,17 @@ from web_app.routes.stats_routes import stats_routes
 
 #DATABASE_URI = "sqlite:///web_app_99.db" # using relative filepath
 #DATABASE_URI = "sqlite:////Users/Username/Desktop/your-repo-name/web_app_99.db" # using absolute filepath on Mac (recommended)
-DATABASE_URI = "sqlite:///C:\\Users\\Z Dubs\\lambda\\twitoff-14-master\\twitoff_development_14.db" # using absolute filepath on Windows (recommended) h/t: https://stackoverflow.com/a/19262231/670433
+#DATABASE_URI = "sqlite:///C:\\Users\\Z Dubs\\lambda\\twitoff-14-master\\twitoff_development_14.db"
+DATABASE_URL =  os.getenv("DATABASE_URL")
+
+SECRET_KEY = os.getenv("SECRET_KEY", default="super secret")
 
 def create_app():
     app = Flask(__name__)
+    app.config["SECRET_KEY"] = SECRET_KEY
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     migrate.init_app(app, db)
 
